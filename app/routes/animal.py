a7cbf0a14b5b1d8f3a0cc448a72ed06fd148fdc0
tildeee <simon.desal@adadevelopersacademy.org>
2023-04-21 11:08:24 -0700

    pulls out validate_animal helper from handle_animal route

--- app/routes/animal.py
+++ app/routes/animal.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Animal():
     def __init__(self, id, species, name, habitat):
@@ -13,6 +13,17 @@ sapphire_animals = [
     Animal(3, "Unicorn", "Not Charlie", "Youtube")
 ]
 
+def validate_animal(animal_id):
+    try:
+        animal_id = int(animal_id)
+    except:
+        abort(make_response({'msg': f"Invalid id '{animal_id}'"}, 400))
+
+    for animal in sapphire_animals:
+        if animal.id == animal_id:
+            return animal
+    return abort(make_response({'msg': f"No animal with id {animal_id}"}, 404))
+
 animals_bp = Blueprint("animals", __name__, url_prefix="/animals")
 
 @animals_bp.route("", methods=['GET'])
@@ -20,16 +31,10 @@ def handle_animals():
     sapphire_animals_as_dict = [vars(animal) for animal in sapphire_animals]
     return jsonify(sapphire_animals_as_dict), 200
 
-@animals_bp.route("/<animal_id>", methods=['GET'])
+@animals_bp.route("/<animal_id>", methods=["GET"])
 def handle_animal(animal_id):
-    try:
-        animal_id = int(animal_id)
-    except:
-        return {'msg': f"Invalid id '{animal_id}'"}, 400
-    for animal in sapphire_animals:
-        if animal.id == animal_id:
-            return {
-                'id': animal.id,
-                'name': animal.name
-            }, 200
-    return {'msg': f"No animal with id {animal_id}"}, 404
+    animal = validate_animal(animal_id)
+    return {
+        "id": animal.id,
+        "name": animal.name
+    }, 200
