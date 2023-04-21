2573c3c794d7df40f7d0d85353f56787929620e2
tildeee <simon.desal@adadevelopersacademy.org>
2023-04-21 10:54:21 -0700

    initial implementation of getting one animal route at /animals/id

--- app/routes/animal.py
+++ app/routes/animal.py
@@ -19,3 +19,17 @@ animals_bp = Blueprint("animals", __name__, url_prefix="/animals")
 def handle_animals():
     sapphire_animals_as_dict = [vars(animal) for animal in sapphire_animals]
     return jsonify(sapphire_animals_as_dict), 200
+
+@animals_bp.route("/<variable_that_represents_animal_id_in_path>", methods=['GET'])
+def handle_animal(variable_that_represents_animal_id_in_path):
+    try:
+        animal_id = int(variable_that_represents_animal_id_in_path)
+    except:
+        return {'msg': f"Invalid id '{variable_that_represents_animal_id_in_path}'"}, 400
+    for animal in sapphire_animals:
+        if animal.id == animal_id:
+            return {
+                'id': animal.id,
+                'name': animal.name
+            }, 200
+    return {'msg': f"No animal with id {variable_that_represents_animal_id_in_path}"}, 404
