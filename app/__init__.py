836eb59de950ea6ae03a47310d386c49b9057d7d
tildeee <simon.desal@adadevelopersacademy.org>
2023-04-27 11:08:37 -0700

    configures sqlalchemy and migrate to look at psql db in create_app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
+
+"""
+The responsibility of create_app is to:
+- Connect Flask to our sapphire_flasky_development db
+    - setting up SQLAlchemy and Migrate to do its thing
+- Register our blueprints (our routes)
+"""
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/sapphire_flasky_development"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     #  add our new animals blueprint
     from flask import Blueprint
