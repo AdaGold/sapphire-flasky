8e0fa29052d4584b17637960050b72ff3b04ac50
goeun <goeuntothepark@gmail.com>
2023-05-08 11:05:55 -0700

    Created a Santuary model

---
+++ app/models/sanctuary.py
@@ -0,0 +1,8 @@
+from app import db
+
+# "one" side, one Sanctuary has many Animals
+class Sanctuary(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80))
+    animals = db.relationship("Animal")
+
